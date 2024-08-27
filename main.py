c448300c32decc3f99b048882ecf1f7444d2823f
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-08-28 00:00:46 +0200

    Testing player

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import sys
+from can import Bus
 from lib.parser import ParserTxt
-from lib.parser import player
+from lib.player import LogPlayer, PlayerParams
+from time import sleep
 
 if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    print("Pass data log filename as first and only argument")
+    sys.exit(1)
+
   filename = sys.argv[1]
-  p = ParserTxt(filename)
+  parser = ParserTxt(filename)
+  
+  params = PlayerParams(
+    (1, 6, 16),
+    {'a': (1, 2, 3)},
+    {'a': 0x00},
+    {'a': lambda x: x},
+    # Bus(interface="socketcan", channel="vcan0", bitrate=1e6)
+  )
+  player = LogPlayer(params)
+  player.start()
+  while (True): ...
