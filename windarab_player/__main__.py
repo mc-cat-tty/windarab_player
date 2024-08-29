5813beb28895998f397d0b11e506f550ab660db7
mc-cat-tty <44820563+mc-cat-tty@usres.noreply.github.com>
2024-08-29 02:55:29 +0200

    Move channels configuration to ad-hoc file

--- windarab_player/__main__.py
+++ windarab_player/__main__.py
@@ -1,9 +1,9 @@
 import sys
-import windarab_player.utils as utils
-from can import Bus
-from windarab_player.parser import ParserTxt
-from windarab_player.player import LogPlayer, PlayerParams, ChannelInfo
 import pygame
+from can import Bus
+from windarab_player.channels_config import CHANNELS_CONFIG
+from windarab_player.parser import ParserTxt
+from windarab_player.player import LogPlayer, PlayerParams
 
 def dispatch_event(event):
   if event.type == pygame.QUIT: sys.exit(1)
@@ -19,21 +19,7 @@ if __name__ == "__main__":
   params = PlayerParams(
     time_points = parser.get_samples()['xtime'],
     channel_samples = parser.get_samples(),
-    channels = {
-      'IMU_LAT': ChannelInfo(
-        0x471,
-        lambda data: utils.float_to_ecu_int(data, 16777216) << 32
-      ),
-      'IMU_LONG': ChannelInfo(
-        0x471,
-        lambda data: utils.float_to_ecu_int(data, 8388608)
-      ),
-      'nmot': ChannelInfo(
-        0x702,
-        lambda data: data
-      ),
-      
-    },
+    channels = CHANNELS_CONFIG,
     can_interface = Bus(interface="socketcan", channel="can0", bitrate=1e6)
   )
 
---
+++ windarab_player/channels_config.py
@@ -0,0 +1,26 @@
+from windarab_player.player import ChannelInfo
+import windarab_player.utils as utils
+
+
+CHANNELS_CONFIG = {
+    'IMU_LAT': ChannelInfo(
+        0x471,
+        lambda data: utils.float_to_ecu_int(data, 16777216, 4)
+    ),
+    'IMU_LONG': ChannelInfo(
+        0x471,
+        lambda data: utils.float_to_ecu_int(data, 8388608)
+    ),
+    'nmot': ChannelInfo(
+        0x702,
+        lambda data: data
+    ),
+    'vwheel_fl': ChannelInfo(
+        0x702,
+        lambda data: utils.float_to_ecu_short(data, 100, 2)
+    ),
+    'ath': ChannelInfo(
+        0x702,
+        lambda data: utils.float_to_ecu_short(data, 100, 6)
+    )
+}
