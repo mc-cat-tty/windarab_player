c6751abfc3b0bcffaaa08f9e1f50839c5560e21a
mc-cat-tty <44820563+mc-cat-tty@usres.noreply.github.com>
2024-08-29 02:55:49 +0200

    Rewrite utils functions

--- windarab_player/utils.py
+++ windarab_player/utils.py
@@ -7,5 +7,17 @@ def transpose(matrix: list[list]):
 def eval_literal_str(literal: str):
   return ast.literal_eval(literal.title())
 
-def float_to_ecu_int(val: float, ratio: int) -> int:
-  return int.from_bytes(pack(">i", int(val * ratio)), byteorder='big')
+def align(data, alignment_bytes: int = 0):
+  return data << (alignment_bytes * 8)
+
+def float_to_ecu(format: str, val: float, ratio: int, byte_align: int = 0) -> int:
+  return align(
+    int.from_bytes(pack(f">{format}", int(val * ratio)), byteorder='big'),
+    byte_align
+  )
+
+def float_to_ecu_int(val: float, ratio: int, byte_align: int = 0) -> int:
+  return float_to_ecu("i", val, ratio, byte_align)
+
+def float_to_ecu_short(val: float, ratio: int, byte_align: int = 0) -> int:
+  return float_to_ecu("h", val, ratio, byte_align)
